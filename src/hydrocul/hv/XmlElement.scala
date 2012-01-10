8f8a07ff123700a62259681edc908aad4d362fd5
hydrocul <hydrocul@gmail.com>
2012-01-11 00:20:34 +0900

    copy hu.xmlelem.Element to hv.XmlElement

---
+++ src/hydrocul/hv/XmlElement.scala
@@ -0,0 +1,72 @@
+package hydrocul.hv;
+
+import scala.collection.IndexedSeqLike;
+import scala.collection.mutable.Builder;
+import scala.collection.mutable.LazyBuilder
+
+import org.jsoup;
+
+trait XmlElement {
+
+  def select(query: String): XmlElements;
+
+  def html: String;
+
+  def text: String;
+
+  def attr(name: String): String;
+
+  override def toString(): String = html;
+
+}
+
+trait XmlElements extends IndexedSeq[XmlElement] {
+
+  def select(query: String): hydrocul.hv.XmlElements;
+
+}
+
+object XmlElement {
+
+  def parseHtml(html: String): XmlElement =
+    new XmlElementImpl(jsoup.parser.Parser.parse(html, "").body);
+
+}
+
+private[hv] class XmlElementImpl(val elem: jsoup.nodes.Element) extends XmlElement {
+
+  def select(query: String): XmlElements = {
+    new XmlElementsImpl(elem.select(query));
+  }
+
+  def html: String = elem.html;
+
+  def text: String = elem.text;
+
+  def attr(name: String): String = elem.attr(name);
+
+}
+
+private[hv] class XmlElementsImpl(elems: jsoup.select.Elements) extends XmlElements
+    with IndexedSeq[XmlElementImpl] with IndexedSeqLike[XmlElementImpl, XmlElementsImpl] {
+
+  def select(query: String): hydrocul.hv.XmlElements =
+    new XmlElementsImpl(elems.select(query));
+
+  def apply(index: Int): XmlElementImpl = new XmlElementImpl(elems.get(index));
+
+  def length: Int = elems.size;
+
+  override def seq = this;
+
+  override protected[this] def newBuilder: Builder[XmlElementImpl, XmlElementsImpl] = {
+    import scala.collection.JavaConverters._;
+    new LazyBuilder[XmlElementImpl, XmlElementsImpl]{
+      override def result: XmlElementsImpl = {
+        new XmlElementsImpl(new jsoup.select.Elements(parts.toIterable.
+          flatMap(_.toIterable).toBuffer.map { e: XmlElementImpl => e.elem; }.asJava));
+      }
+    }
+  }
+
+}
--- src/hydrocul/hv/http/HtmlPage.scala
+++ src/hydrocul/hv/http/HtmlPage.scala
@@ -1,7 +1,9 @@
 package hydrocul.hv.http;
 
+/*
 class HtmlPage private[http] (response: Response, url: String)
     extends Page(response, url) {
 
 }
+*/
 
