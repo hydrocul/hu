08ad23d97edddc7d9b87c68972cb3203323c18da
hydrocul <hydrocul@gmail.com>
2012-01-22 22:27:16 +0900

    implement hv.CipherUtil.binaryToSha256

--- src/hydrocul/hv/CipherUtil.scala
+++ src/hydrocul/hv/CipherUtil.scala
@@ -27,25 +27,54 @@ object CipherUtil {
     EncodingMania.decodeHex(input);
   }
 
-  def binaryToHash(data: Array[Byte]): Array[Byte] = {
-    val sha1 = MessageDigest.getInstance("SHA-1");
-    sha1.reset();
-    sha1.update(data);
-    val key = sha1.digest();
+  def binaryToSha1(data: Array[Byte]): Array[Byte] = {
+    val shax = MessageDigest.getInstance("SHA-1");
+    shax.reset();
+    shax.update(data);
+    val key = shax.digest();
     if(key.length != 20){
       throw new AssertionError(key.map("%02x".format(_)).mkString);
     }
     key;
   }
 
+  def binaryToSha256(data: Array[Byte]): Array[Byte] = {
+    val shax = MessageDigest.getInstance("SHA-256");
+    shax.reset();
+    shax.update(data);
+    val key = shax.digest();
+    if(key.length != 32){
+      throw new AssertionError(key.map("%02x".format(_)).mkString);
+    }
+    key;
+  }
+
+  def binaryToHash(data: Array[Byte]): Array[Byte] = binaryToSha1(data);
+
   private[hv] def test(): Seq[Option[String]] = {
     import TestLib._;
-    val a = encodeCipher("abc".getBytes, "あいうえお".getBytes("UTF-8"));
-    val b = new String(decodeCipher("abc".getBytes, a), "UTF-8");
-    List(
-      assertEquals("07b91211306447be8244146ad5d07a03", encodeHex(a)),
-      assertEquals("あいうえお", b)
-    );
+    {
+      val a = encodeCipher("abc".getBytes, "あいうえお".getBytes("UTF-8"));
+      val b = new String(decodeCipher("abc".getBytes, a), "UTF-8");
+      List(
+        assertEquals("07b91211306447be8244146ad5d07a03", encodeHex(a)),
+        assertEquals("あいうえお", b)
+      );
+    } ++
+    {
+      val actual = encodeHex(binaryToSha256("abc\n".getBytes));
+      val expected = "edeaaff3f1774ad2888673770c6d64097e391bc362d7d6fb34982ddf0efd18cb";
+      List(
+        assertEquals(expected, actual)
+      );
+    } ++
+    {
+      val actual = encodeHex(binaryToSha256("1234".getBytes));
+      val expected = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4";
+      List(
+        assertEquals(expected, actual)
+      );
+    }
   }
 
 }
