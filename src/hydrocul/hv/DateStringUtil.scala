5cf98d0663f43ff6e617961e33783e7f8ce7c186
hydrocul <hydrocul@gmail.com>
2012-02-11 22:25:15 +0900

    update hv.DateStringUtil

--- src/hydrocul/hv/DateStringUtil.scala
+++ src/hydrocul/hv/DateStringUtil.scala
@@ -4,9 +4,8 @@ object DateStringUtil {
 
   def parse(dateTimeStr: String): Long = {
     List(
-      "EEE, dd MMM yyyy hh:mm:ss zzz", // RFC1123
+      "EEE, dd MMM yyyy HH:mm:ss zzz", // RFC1123
       "EEE MMM dd hh:mm:ss zzz yyyy"
-      // "EEE, MMM dd hh:mm:ss Z yyyy"
     ).foldLeft[Option[Long]](None){ (o, f) => o.orElse {
       val format = new java.text.SimpleDateFormat(f, java.util.Locale.US);
       try {
@@ -28,7 +27,9 @@ object DateStringUtil {
       assertEquals(86400L * 1000,
         parse("Thu Jan 02 00:00:00 +0000 1970")),
       assertEquals(1328612715000L,
-        parse("Tue Feb 07 11:05:15 +0000 2012"))
+        parse("Tue Feb 07 11:05:15 +0000 2012")),
+      assertEquals(1328612715000L,
+        parse("Tue Feb 07 20:05:15 GMT+09:00 2012"))
     );
   }
 
