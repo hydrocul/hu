1f85a7ac6b7e5a5513f3a8151798bc2701f2e75a
hydrocul <hydrocul@gmail.com>
2012-03-12 00:09:55 +0900

    update hv.wwget.Main

--- src/hydrocul/hv/wwget/CmdOpt.scala
+++ src/hydrocul/hv/wwget/CmdOpt.scala
@@ -1,35 +1,104 @@
 package hydrocul.hv.wwget;
 
+import hydrocul.hv.http.UrlInfo;
+
+/**
+ * コマンドラインオプションの情報を保存するクラス。
+ */
 private[wwget] case class CmdOpt (
-  url: String
-);
+  url: String,
+  output: CmdOpt.Output,
+  selector: Option[CmdOpt.Selector]
+){
+  override def toString(): String = {
+    url; // TODO
+  }
+}
 
 private[wwget] object CmdOpt {
 
   def apply(args: List[String]): CmdOpt =
-    CmdOptBuilder(None).parse(args);
+    CmdOptBuilder(None, None, None).parse(args);
+
+  sealed trait Output;
+
+  case class FileOutput (
+    fileName: String
+  ) extends Output;
+
+  object StdOutput extends Output;
+
+  case class Selector (
+    selector: String,
+    content: Selector.Content
+  );
+
+  object Selector {
+
+    sealed trait Content;
+
+//    object OuterHtml extends Content;
+
+//    object InnerHtml extends Content;
+
+    object Text extends Content;
+
+//    case class Attribute(name: String) extends Content;
+
+  }
 
 }
 
 private[wwget] case class CmdOptBuilder (
-  url: Option[String]
+  url: Option[String],
+  output: Option[CmdOpt.Output],
+  selector: Option[CmdOpt.Selector]
 ){
 
   def parse(args: List[String]): CmdOpt = {
     (this, args) match {
       case (_, Nil) => build;
-      case (CmdOptBuilder(None), url :: tail) =>
-        CmdOptBuilder(Some(url)).parse(tail);
+      case (CmdOptBuilder(url, None, selector), "-O" :: ofname :: tail) =>
+        CmdOptBuilder(url, Some(CmdOpt.FileOutput(ofname)), selector).parse(tail);
+      case (CmdOptBuilder(url, None, selector), "--stdout" :: tail) =>
+        CmdOptBuilder(url, Some(CmdOpt.StdOutput), selector).parse(tail);
+      case (CmdOptBuilder(url, ofname, None), "--selector" :: selector :: tail) =>
+        CmdOptBuilder(url, ofname,
+          Some(CmdOpt.Selector(selector, CmdOpt.Selector.Text))).parse(tail);
+      case (CmdOptBuilder(None, ofname, selector), url :: tail) =>
+        CmdOptBuilder(Some(url), ofname, selector).parse(tail);
       case _ => throw new IllegalArgumentException(args.toString);
     }
   }
 
   private def build: CmdOpt = {
     this match {
-      case CmdOptBuilder(Some(url)) => CmdOpt(url);
+      case CmdOptBuilder(Some(url), None, selector) =>
+        CmdOpt(url, CmdOpt.FileOutput(CmdOptBuilder.urlToOutputFileName(url)), selector);
+      case CmdOptBuilder(Some(url), Some(output), selector) =>
+        CmdOpt(url, output, selector);
       case _ => throw new IllegalArgumentException(this.toString);
     }
   }
 
 }
 
+private[wwget] object CmdOptBuilder {
+
+  private def urlToOutputFileName(url: String): String = {
+
+    val UrlPattern1 = ".*/([^/?]*)".r;
+    val UrlPattern2 = ".*/([^/?]*)?(.*)".r;
+
+    UrlInfo(url).path match {
+      case "" => "index.html";
+      case UrlPattern1("") => "index.html";
+      case UrlPattern1(fname) => fname;
+      case UrlPattern2("", _) => "index.html";
+      case UrlPattern2(fname, _) => fname;
+      case path => throw new AssertionError(path);
+    }
+
+  }
+
+}
--- src/hydrocul/hv/wwget/Main.scala
+++ src/hydrocul/hv/wwget/Main.scala
@@ -1,47 +1,61 @@
 package hydrocul.hv.wwget;
 
+import hydrocul.hv.EncodingMania;
 import hydrocul.hv.File;
+import hydrocul.hv.http.HtmlPage;
 import hydrocul.hv.http.WebBrowser;
-import hydrocul.hv.http.UrlInfo;
 
 object Main {
 
   def main(args: Array[String]){
-    val cmdOpt = CmdOpt(args.toList);
-    exec(cmdOpt);
+    val argsList = args.toList;
+    if(argsList == "--help" :: Nil){
+      help();
+    } else {
+      val cmdOpt = CmdOpt(argsList);
+      exec(cmdOpt);
+    }
   }
 
   def exec(cmdOpt: CmdOpt){
 
-    val fname = outputFileName(cmdOpt);
-
-    val file = File(fname);
-    if(file.exists){
-      throw new IllegalStateException("File already exists: " + fname);
+    val output: Array[Byte] => Unit = cmdOpt.output match {
+      case CmdOpt.FileOutput(fname) =>
+        val file = File(fname);
+        if(file.exists){
+          throw new IllegalStateException("File already exists: " + fname);
+        }
+        { bin: Array[Byte] =>
+          file.write(Some(bin));
+        }
+      case CmdOpt.StdOutput =>
+        { bin: Array[Byte] =>
+          System.out.write(bin);
+        }
     }
 
     val browser = WebBrowser.create();
     val page = browser.doGet(cmdOpt.url);
-    val body = page.response.body;
 
-    file.write(Some(body));
+    cmdOpt.selector match {
+
+      case None =>
+        val body = page.response.body;
+        output(body);
+
+      case Some(selector: CmdOpt.Selector) =>
+        val element = page.asInstanceOf[HtmlPage].select(selector.selector);
+        val text = element.text;
+        output(EncodingMania.encodeChar(text, "UTF-8"));
+
+    }
 
   }
 
-  private def outputFileName(cmdOpt: CmdOpt): String = {
-
-    val UrlPattern1 = ".*/([^/?]*)".r;
-    val UrlPattern2 = ".*/([^/?]*)?(.*)".r;
-
-    UrlInfo(cmdOpt.url).path match {
-      case "" => "index.html";
-      case UrlPattern1("") => "index.html";
-      case UrlPattern1(fname) => fname;
-      case UrlPattern2("", _) => "index.html";
-      case UrlPattern2(fname, _) => fname;
-      case path => throw new AssertionError(path);
-    }
-
+  def help(){
+    println("""
+wwget [option] url
+""".trim);
   }
 
 }
