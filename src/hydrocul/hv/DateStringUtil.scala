0c5ac65634495af39c7902610c8dba87f9efe456
hydrocul <hydrocul@gmail.com>
2012-03-14 20:45:36 +0900

    hv.DateStringUtil の対応フォーマットを追加

--- src/hydrocul/hv/DateStringUtil.scala
+++ src/hydrocul/hv/DateStringUtil.scala
@@ -5,7 +5,8 @@ object DateStringUtil {
   def parse(dateTimeStr: String): Long = {
     List(
       "EEE, dd MMM yyyy HH:mm:ss zzz", // RFC1123
-      "EEE MMM dd hh:mm:ss zzz yyyy"
+      "EEE MMM dd HH:mm:ss zzz yyyy",
+      "dd/MMM/yyy:HH:mm:ss zzz"
     ).foldLeft[Option[Long]](None){ (o, f) => o.orElse {
       val format = new java.text.SimpleDateFormat(f, java.util.Locale.US);
       try {
@@ -29,7 +30,11 @@ object DateStringUtil {
       assertEquals(1328612715000L,
         parse("Tue Feb 07 11:05:15 +0000 2012")),
       assertEquals(1328612715000L,
-        parse("Tue Feb 07 20:05:15 GMT+09:00 2012"))
+        parse("Tue Feb 07 20:05:15 GMT+09:00 2012")),
+      assertEquals(1328612715000L,
+        parse("07/Feb/2012:20:05:15 +0900")),
+      assertEquals(1328583915000L,
+        parse("07/Feb/2012:12:05:15 +0900"))
     );
   }
 
